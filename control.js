2414fa35809a748773e34ceed0c9d9b86355ba35
Diego <diegolara7820@gmail.com>
2024-11-21 06:28:02 -0500

    nuevo btn

--- control.js
+++ control.js
@@ -18,7 +18,7 @@ function calculateChecksum(data) {
 
 // Función para enviar comando DYD#
 function sendDYDCommand(callback) {
-    const command = Buffer.from('DYD#');
+    const command = Buffer.from('DYD#', 'ascii');
     const header = Buffer.from([0x78, 0x78]);
     const protocol = Buffer.from([0x80]);
     const length = Buffer.from([command.length + 5]);
@@ -58,6 +58,48 @@ function sendDYDCommand(callback) {
     });
 }
 
+// Función para enviar comando HFYD#
+function sendHFYDCommand(callback) {
+    const command = Buffer.from('HFYD#', 'ascii');
+    const header = Buffer.from([0x78, 0x78]);
+    const protocol = Buffer.from([0x80]);
+    const length = Buffer.from([command.length + 5]);
+    const serial = Buffer.from([0x00, 0x01]);
+    const checksum = calculateChecksum([...protocol, ...command, ...serial]);
+    const footer = Buffer.from([0x0D, 0x0A]);
+
+    const packet = Buffer.concat([
+        header,
+        length,
+        protocol,
+        command,
+        serial,
+        Buffer.from([checksum]),
+        footer,
+    ]);
+
+    const client = new net.Socket();
+    client.connect(DEVICE_PORT, DEVICE_IP, () => {
+        console.log('Conectado al GPS. Enviando comando HFYD#...');
+        client.write(packet);
+    });
+
+    client.on('data', (data) => {
+        console.log('Respuesta del GPS:', data.toString('hex'));
+        callback(null, data.toString('hex'));
+        client.destroy();
+    });
+
+    client.on('close', () => {
+        console.log('Conexión cerrada.');
+    });
+
+    client.on('error', (err) => {
+        console.error('Error:', err.message);
+        callback(err, null);
+    });
+}
+
 // Configuración de Express y Socket.IO
 app.use(express.static(__dirname + '/public'));
 
@@ -66,6 +108,16 @@ io.on('connection', (socket) => {
 
     socket.on('send-command', () => {
         sendDYDCommand((err, response) => {
+            if (err) {
+                socket.emit('response', `Error: ${err.message}`);
+            } else {
+                socket.emit('response', `Respuesta del GPS: ${response}`);
+            }
+        });
+    });
+
+    socket.on('send-hfyd-command', () => {
+        sendHFYDCommand((err, response) => {
             if (err) {
                 socket.emit('response', `Error: ${err.message}`);
             } else {
